bca8bbe7e0d3389fb08276c0d8cd1a90de15ab99
Erich Behrens <me@eb1.it>
2017-12-14 23:02:03 +0100

    On scroll component

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # react-on-scroll
 
+[![Travis](https://img.shields.io/travis/erichbehrens/react-on-scroll.svg)](https://travis-ci.org/erichbehrens/react-on-scroll)
+[![dependencies](https://david-dm.org/erichbehrens/react-on-scroll.svg)](https://david-dm.org/erichbehrens/react-on-scroll)
+[![devDependencies](https://david-dm.org/erichbehrens/react-on-scroll/dev-status.svg)](https://david-dm.org/erichbehrens/react-on-scroll?type=dev)
+[![downloads](https://img.shields.io/npm/dt/react-on-scroll.svg)](https://www.npmjs.com/package/react-on-scroll)
+
 React to scroll events: animate components, lazy load data, infinite scroll
---
+++ src/index.js
@@ -0,0 +1,82 @@
+import React from 'react';
+
+function getElementPosition(element) {
+	const rect = element.getBoundingClientRect();
+	const html = document.documentElement;
+	const viewportBottom = window.innerHeight || html.clientHeight;
+	return {
+		fromTop: {
+			top: rect.top,
+			bottom: rect.bottom,
+		},
+		fromBottom: {
+			top: viewportBottom - rect.top,
+			bottom: viewportBottom - rect.bottom,
+		},
+	};
+}
+
+class OnScroll extends React.Component {
+	constructor(props) {
+		super(props);
+		this.triggers = props.triggers.map(trigger => ({
+			repeat: 0,
+			...trigger,
+			executionCount: 0,
+			lastState: false,
+		}));
+	}
+
+	onScroll = () => {
+		const position = getElementPosition(this.element);
+		const { triggerBase } = this.props;
+		const nextTriggers = this.triggers.map((trigger) => {
+			const currentState = (!trigger.bottom || trigger.bottom < position.fromBottom[triggerBase])
+				&& (!trigger.top || trigger.top < position.fromTop[triggerBase]);
+			if (currentState !== trigger.lastState) {
+				trigger.callback(currentState);
+				const executionCount = (trigger.executionCount || 0) + 1;
+				const repeat = (trigger.repeat === 0 || executionCount < trigger.repeat);
+				return repeat && { ...trigger, lastState: currentState, executionCount };
+			}
+			return trigger;
+		});
+		this.triggers = nextTriggers.filter(item => item);
+		if (this.triggers.length === 0) {
+			this.unsubscribe();
+		}
+		this.handlingScroll = false;
+	}
+
+	handlingScroll = false;
+	handleScroll = () => {
+		this.handlingScroll = this.handlingScroll || requestAnimationFrame(this.onScroll);
+	}
+
+	unsubscribe() {
+		if (typeof window !== 'undefined' && window.removeEventListener) {
+			window.removeEventListener('scroll', this.handleScroll);
+		}
+	}
+
+	componentDidMount() {
+		if (typeof window !== 'undefined' && window.addEventListener) {
+			window.addEventListener('scroll', this.handleScroll);
+		}
+	}
+
+	componentWillUnmount() {
+		this.unsubscribe();
+	}
+
+	render() {
+		return <div
+			ref={(element) => { this.element = element; }}
+			className={this.props.className}
+		>
+			{this.props.children}
+		</div>;
+	}
+}
+
+export default OnScroll;
